7949f401890066d4e6f79b2e7449f4461b62fcc8
Kwansui <kwansui369@gmail.com>
2015-09-25 15:10:00 +0200

    updated php

--- Desktop/accessor.php
+++ Desktop/accessor.php
@@ -5,12 +5,13 @@
 	$password = "";
 	//$password = "root";
 	$host = "localhost";
-	$db = "popcircle";
+	$db = "personalAssistant";
 	$link = mysqli_connect($host, $name, $password, $db) or die("Failed to connect.".mysqli_error($link));
+	$today = getdate();
 	
 	function loginCheck($link, $uname, $pw) 	//selects the correct table and data for login check
 	{
-		$users = mysqli_query($link, "SELECT * FROM members WHERE (username = '$uname') AND (password = '$pw')")or die("Failed to fetch users.".mysqli_error($link));
+		$users = mysqli_query($link, "SELECT * FROM users WHERE (username = '$uname') AND (password = '$pw')")or die("Failed to fetch users.".mysqli_error($link));
 		
 		$userInfo = mysqli_fetch_array($users);
 		$numUsers = mysqli_num_rows($users);
@@ -25,13 +26,44 @@
 			return false;
 	}
 	
-	function showEvents($link, $uname) //assuming all events are in one table called "events" where we need to select them by username "field"
+	function showTodo($link, $uname) 
 	{
-		$events = mysqli_query($link, "SELECT * FROM events WHERE (username = '$uname')")or die("Failed to fetch events.".mysqli_error($link));
+		$unchecked = mysqli_query($link, "SELECT * FROM tasks WHERE (username = '$uname') AND (checked = 0)");
+		$checked = mysqli_query($link, "SELECT * FROM tasks WHERE (username = '$uname') AND (checked = 1)");
+		$numUnchecked = mysqli_fetch_array($unchecked);
+		$numChecked = mysqli_fetch_array($checked);
 		
-		while ($item = mysqli_fetch_array($items))
+		if ($numUnchecked > 0)
 		{
-			
+			while ($item = mysqli_fetch_array($unchecked))
+			{
+				//echo task name as html
+			}
+		}
+		
+		if ($numChecked > 0)
+		{
+			//maybe output a line of separation between unchecked and checked
+			while ($item = mysqli_fetch_array($checked))
+			{
+				//echo each task name as html
+			}
+		}
+	}
+	
+	function showEvent($link, $uname)
+	{	//this code is psuedo code for the convert part
+		$data = mysqli_query($link, "SELECT * FROM events WHERE (username = '$uname') AND convert(time.month) ORDER BY time ASC");
+		
+		while ($item = mysqli_fetch_array($data))
+		{
+			//echo all details of item as html
+			//title
+			//time[hh:mm]
+			//time.date[dd:mm:yyyy]
+			//venue
+			//reminders
+			//busy/avaiable
 		}
 	}
 ?>
